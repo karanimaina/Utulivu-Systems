6aa531b7b46c904f79c70ea8cbb97ab92cf28fd9
karanimaina <karani.maina2010@gmail.com>
2023-05-11 12:08:39 +0300

    first commit

---
+++ src/main/java/com/example/utulivusystems/UtulivuSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.example.utulivusystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtulivuSystemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtulivuSystemsApplication.class, args);
+    }
+
+}
